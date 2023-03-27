ffee4f75df7f816219c3d31198bf588a24a6211e
Fabio <fabiolima@fabiolima.dev>
2023-03-27 06:50:00 -0300

    Layout básico da página

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    colors: {
+      primary: "#EBDCE3",
+      secondary: "#32352A",
+      tertiary: "#ffffff",
+      quaternary: "#E9A888",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat"],
+      },
+    },
+  },
+  plugins: [],
+};
